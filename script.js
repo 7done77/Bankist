8524c8ac30d6abf97f5ca8fabf5666401ab5205a
Gugalomi=geo <iva290386@gmail.com>
2025-09-21 22:36:53 +0400

    Add login and transfer money functions with js

--- script.js
+++ script.js
@@ -76,41 +76,32 @@ const displayMovements = function (movements) {
   })
 }
 
-displayMovements(account1.movements)
-
-const calcDisplayBalance = function (movements) {
-  const balance = movements.reduce((acc, cur) => acc + cur, 0)
-  labelBalance.textContent = `${balance} 💶`
+const calcDisplayBalance = function (acc) {
+  acc.balance = acc.movements.reduce((acc, cur) => acc + cur, 0)
+  labelBalance.textContent = `${acc.balance} 💶`
 }
 
-calcDisplayBalance(account1.movements)
-
-const calcDisplaySummary = function (movements) {
-  const incomes = movements
+const calcDisplaySummary = function (acc) {
+  const incomes = acc.movements
     .filter((mov) => mov > 0)
     .reduce((acc, mov) => acc + mov, 0)
 
   labelSumIn.textContent = `${incomes}💶`
 
-  const out = movements
+  const out = acc.movements
     .filter((mov) => mov < 0)
     .reduce((acc, mov) => acc + mov, 0)
 
   labelSumOut.textContent = `${Math.abs(out)}💶`
 
-  const interest = movements
+  const interest = acc.movements
     .filter((mov) => mov > 0)
-    .map((deposit) => (deposit * 1.2) / 100)
-    .filter((int, i, arr) => {
-      console.log(arr)
-      return int >= 1
-    })
+    .map((deposit) => (deposit * acc.interestRate) / 100)
+    .filter((int) => int >= 1)
     .reduce((acc, int) => acc + int, 0)
 
   labelSumInterest.textContent = `${interest}💶`
 }
-
-calcDisplaySummary(account1.movements)
 
 const createUserNames = function (accs) {
   accs.forEach(function (acc) {
@@ -123,3 +114,77 @@ const createUserNames = function (accs) {
 }
 
 createUserNames(accounts)
+
+const updateUI = function (acc) {
+  // Display movements
+  displayMovements(acc.movements)
+
+  // Display balance
+  calcDisplayBalance(acc)
+
+  // Display summary
+  calcDisplaySummary(acc)
+}
+
+// Event handler
+let currentAccount
+
+btnLogin.addEventListener('click', function (e) {
+  // Prevent form from submitting
+  e.preventDefault()
+
+  currentAccount = accounts.find(
+    (acc) => acc.username === inputLoginUsername.value
+  )
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    // Display UI and welcome message
+    labelWelcome.textContent = `Welcome back ${
+      currentAccount.owner.split(' ')[0]
+    }`
+    containerApp.style.opacity = 100
+
+    // clear input fields
+    inputLoginUsername.value = inputLoginPin.value = ''
+    inputLoginPin.blur()
+
+    updateUI(currentAccount)
+  }
+})
+
+btnTransfer.addEventListener('click', function (e) {
+  e.preventDefault()
+
+  const amount = Number(inputTransferAmount.value)
+  const recieverAcc = accounts.find(
+    (acc) => acc.username === inputTransferTo.value
+  )
+  inputTransferAmount.value = inputTransferTo.value = ''
+
+  if (
+    amount > 0 &&
+    recieverAcc &&
+    currentAccount.balance >= amount &&
+    recieverAcc?.username !== currentAccount.username
+  ) {
+    currentAccount.movements.push(-amount)
+    recieverAcc.movements.push(amount)
+    updateUI(currentAccount)
+  }
+})
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault()
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      (acc) => acc.username === currentAccount.username
+    )
+    console.log(index)
+
+    accounts.splice(index, 1)
+  }
+})
+
+// Lectures
